241500349a69eacdb3da2d308d2edeee0514934a
Ali-MHoseini <ali.mhoseini99@gmail.com>
2023-04-06 21:49:54 +0430

    build dashboard and customers and stuffs page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Route, Routes} from 'react-router-dom'
 import {Login} from './pages/pages'
+import {Dashboard} from "./pages/Dashboard/Dashboard";
+import {Customers} from "./pages/Customers/Customers";
+import {Stuffs} from "./pages/Stuffs/Stuffs";
 function App() {
   return (
     <>
         <Routes>
             <Route path='/' element={<Login />}/>
-            {/*<Route path='/cart' element={} />*/}
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/dashboard/customers' element={<Customers />} />
+            <Route path='/dashboard/stuffs' element={<Stuffs />} />
             {/*    <Route path='*'/>*/}
         </Routes>
     </>
